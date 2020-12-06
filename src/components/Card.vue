901dd24f4317e9b709f8a6c6fe3c41e6b15c088d
yehyitung <joe66033166@gmail.com>
2020-12-07 01:18:52 +0800

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="card-wrap" @click="returnText">
+    <div class="card-top-wrapper">
+      <div class="card-title">{{ title }}</div>
+      <slot name="favorite">預設收藏標題</slot>
+    </div>
+    <div class="card-content">
+      <img class="card-img" :src="imgSrc">
+      <div class="card-textwrap">
+        <div class="card-city">{{ city }}</div>
+        <div class="card-name">{{ name }}</div>
+        <div class="card-text">{{ content }}</div>
+        <slot name="subContent">預設副內容</slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  methods:{
+    returnText(){
+      var text = this.name + ":" + this.content;
+     this.$emit('returnCardData',text)
+    }
+  },
+  props:{
+    title:{
+      type:String,
+      default:""
+    },
+    imgSrc:{
+      type:String
+    },
+    city:{
+      type:String,
+      default:""
+    },
+    name:{
+      type:String,
+      default:""
+    },
+    content:{
+      type:String,
+      default:""
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+  .card-wrap{
+    width: 400px;
+    border: 1px solid gray;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    margin-bottom: 20px;
+    .card-top-wrapper{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .card-title{
+        font-weight: 500;
+        font-size: 24px;
+        color: orange;
+        margin-bottom: 10px;
+        margin-right: 20px;
+      }
+      .title-icon{
+        font-weight: 500;
+        font-size: 18px;
+        color: gray;
+        margin-bottom: 10px;
+      }
+    }
+    .card-content{
+      width: 100%;
+      display: flex;
+      .card-img{
+        width: 50%;
+        margin-right: 15px;
+      }
+      .card-textwrap{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        .card-city{
+          font-weight: 500;
+          font-size: 20px;
+        }
+        .card-text{
+          text-align: left;
+          font-size: 16px;
+        }
+      }
+    }
+  
+
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrap">
+      <template v-for="item in cardListData">
+       <Card :key="item.id" :title="item.title" :imgSrc="item.imgSrc" :city="item.city" :name="item.name" :content="item.content" @returnCardData="(e) => getCardData(e)">
+         <template slot="favorite">
+          <span class="title-icon">收藏</span>
+         </template>
+         <template slot="subContent">
+          <span class="title-icon">副內容</span>
+         </template>
+         
+       </Card>
+       
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components:{
+    Card
+  },
+  data(){
+    return{
+      cardListData: []
+    }
+  },
+  mounted(){
+    this.getAnimalData()
+  },
+  methods:{
+    getCardData(e){
+      console.log(e);
+    },
+    getAnimalData(){
+      fetch('./mock.json')
+        .then(res => res.json())
+        .then(res => {
+          this.cardListData = res
+        })
+
+    }
   }
 }
 </script>
+
+<style lang="scss">
+    .wrap{
+  width: 960px;
+  margin: 50px auto;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: space-around;
+}
+</style>
